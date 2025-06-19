0e4bc08e5673a1e1d31a38b56afe43ba399c0cea
gautamdua2892004@gmail.com <gautamdua2892004@gmail.com>
2025-06-19 20:13:34 +0530

    i love u

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'; 
 import Skills from '../assets/Skills.png';
+import { technologies } from "../constants";
+
 
 export const SkillSection = () => {
   return (
-   
-        <section id="about" className="py-24 px-4 relative">
-      {" "}
+    <section id="about" className="py-24 px-4 relative">
       <div className="container mx-auto max-w-5xl">
-        <h2 className="text-3xl md:text-4xl font-bold  text-center">
+        <h2 className="text-3xl md:text-4xl font-bold text-center">
           My <span className="text-primary"> Skills</span>
         </h2>
-        
-            <p className="text-lg text-gray-600">
-                I have experience in various technologies and tools that help me build efficient and scalable applications.
-            </p>
-        <img src={Skills} alt="Skills" className="w-full pt-18 h-[60%]" />
-        <div className="text-center mt-8">
-            
+        <p className="text-lg text-gray-600">
+          I have experience in various technologies and tools that help me build efficient and scalable applications.
+        </p>
+        <div className="flex flex-row flex-wrap justify-center items-center gap-10 mt-6">
+          {technologies.map((technology) => (
+            <div
+              key={technology.name}
+              className="w-16 h-16 rounded-full bg-white/10 dark:bg-gray-800 flex items-center justify-center"
+            >
+              <img
+                src={technology.icon}
+                alt={technology.name}
+                className="w-8 h-8 object-contain"
+              />
+            </div>
+          ))}
         </div>
+        <img src={Skills} alt="Skills" className="w-full mt-4 h-[80%] " />
+        <div className="text-center mt-8"></div>
       </div>
-        </section>
-  
+    </section>
   )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,72 @@
+import{
+    ai,
+    blockchain,
+    css,
+    docker,
+    figma,
+    git,
+    html,
+    javascript,
+    mongodb,
+    nodejs,
+    reactjs,
+    redux,
+    tailwind
+} from "../assets";
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "AI & ML",
+    icon: ai,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "Bloackchain",
+    icon: blockchain,
+  },
+];
+
+export { technologies };
