80a64be28c4f0ec4ef14f4c8805ac4f07f5d03b0
Nivedika-sharma <sharmanivedika6@gmail.com>
2025-06-21 13:11:21 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
- 
+createRoot(document.getElementById('root')).render( 
   <StrictMode>
     <App />
   </StrictMode>,
-   </BrowserRouter>
+  
 )
